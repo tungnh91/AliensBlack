7183b92970e1505c002458f7c317592378f2c607
Tim Nguyen <tungnh91@gmail.com>
2017-03-12 14:20:25 -0700

    Fetching data from db when offline button is clicked

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -17,9 +17,10 @@ class App extends React.Component {
       url: 'http://localhost:3000/items',
       type: 'GET',
       success: (redditData) => {
-        redditData = JSON.parse(redditData);
+        redditData = (JSON.parse(redditData)).data.children;
+        console.log(redditData[0], 'this is what I hoped to be an array', 'is it?', Array.isArray(redditData));
         this.setState({
-          items: redditData.data.children
+          items: redditData
         })
       },
       error: (err) => {
@@ -27,23 +28,24 @@ class App extends React.Component {
       }
     });
   }
-
-  offline() {
+  componentWillReceiveProps(redditData) {
+    console.log('go offline son!');
     $.ajax({
       url: 'http://localhost:3000/offline',
       type: 'GET',
       success: (redditData) => {
-        redditData = JSON.parse(redditData);
-        console.log( redditData,'redditdata-----afterparsed------');
+        console.log( redditData[23],'redditdata-----------','typeof', typeof redditData);
         this.setState({
-          items: redditData.data.children
+          items: redditData
         })
+        console.log(this.state.items, 'current state after loading offline data');
       },
       error: (err) => {
         console.log('err from inside offline method', err);
       }
     });
   }
+
 
   clicked() {
     $.ajax({
@@ -60,6 +62,7 @@ class App extends React.Component {
 
   render () {
     return (<div>
+      <button onClick={this.componentWillReceiveProps.bind(this)}> Go offline son! </button>
       <h1>Aliens Black</h1>
       <h2>Reddit, declustered, <button onClick={this.clicked.bind(this)}> offline. </button></h2>
       <List items={this.state.items}/>
